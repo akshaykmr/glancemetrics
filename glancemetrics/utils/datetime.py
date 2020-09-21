06a4dd5d7dc05b9fc214bb5bf5825765b0436241
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-09-22 02:36:53 +0530

    added docstring for util

--- glancemetrics/utils/datetime.py
+++ glancemetrics/utils/datetime.py
@@ -2,8 +2,9 @@ import datetime
 import re
 
 
-def parse_tz_offset(offset_str) -> datetime.tzinfo:
-    sign, hours, minutes = re.match("([+\-]?)(\d{2})(\d{2})", offset_str).groups()
+def parse_tz_offset(offset: str) -> datetime.tzinfo:
+    """convert str offsets like 0530 and -0700 to tzinfo"""
+    sign, hours, minutes = re.match("([+\-]?)(\d{2})(\d{2})", offset).groups()
     sign = -1 if sign == "-" else 1
     hours, minutes = int(hours), int(minutes)
     return datetime.timezone(sign * datetime.timedelta(hours=hours, minutes=minutes))
