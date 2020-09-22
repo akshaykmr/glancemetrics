28f6cd6ed5cdbcc22452ac841b37f0e9bb59cb4d
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-09-22 16:15:38 +0530

    test factories for domain model

--- test/factories.py
+++ test/factories.py
@@ -1,1 +1,57 @@
+from random import choice, randint
+from datetime import timedelta
 
+from glancemetrics.domain.models import LogRecord, LogBucket
+from glancemetrics.utils.datetime import current_time
+
+
+def random_ip():
+    ips = [
+        "60.147.141.19",
+        "60.147.141.19",
+        "43.2.214.24",
+        "26.88.193.28",
+        "200.255.24.100",
+        "5.76.99.64",
+        "225.254.122.42",
+        "229.137.91.145",
+        "130.206.72.64",
+        "82.65.216.216",
+        "216.219.66.127",
+        "22.156.169.65",
+        "141.71.138.218",
+        "200.137.223.25",
+    ]
+    return choice(ips)
+
+
+def random_method():
+    methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
+    return choice(methods)
+
+
+def random_path():
+    paths = ["/", "/doggo" "/pupper", "/api/hey"]
+    return choice(paths)
+
+
+def log_record_dm(
+    ip=None,
+    time=None,
+    method=None,
+    path=None,
+    status_code=None,
+    content_size=None,
+    identity=None,
+    user_id=None,
+) -> LogRecord:
+    return LogRecord(
+        ip=ip or random_ip(),
+        time=time or current_time() - timedelta(seconds=randint(0, 100)),
+        method=method or random_method(),
+        path=path or random_path(),
+        status_code=status_code or randint(100, 600),
+        content_size=randint(500, 1500) if content_size is None else content_size,
+        identity=identity,
+        user_id=user_id,
+    )
