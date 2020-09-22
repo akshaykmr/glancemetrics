b290dc6e6097d4906f00fa7fbc74f1ae3cbfcf76
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-09-22 10:39:31 +0530

    experimental domain models for log analysis

--- glancemetrics/domain/models.py
+++ glancemetrics/domain/models.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
-from typing import Optional
-from datetime import datetime
+from dataclasses import dataclass, field
+from typing import Optional, List
+from datetime import datetime, timedelta
 from glancemetrics.utils.datetime import parse_tz_offset
 from clfparser import CLFParser
 
@@ -40,3 +40,53 @@ class LogRecord:
     def section(self) -> str:
         bread_crumbs = [c for c in self.path.split("/") if c]
         return bread_crumbs[0] if bread_crumbs else "/"
+
+
+@dataclass
+class LogBucket:
+    """logs captured in a second interval."""
+
+    time: datetime  # time + 1 second interval
+    logs: List[LogRecord] = field(default_factory=list)
+
+    def __post_init__(self):
+        # the grouping is per second
+        assert self.time.microsecond == 0
+
+    def add(self, log: LogRecord):
+        # floors microsecond, note: replace returns new datetime, doesn't modify original
+        log_interval = log.time.replace(microsecond=0)
+        if log_interval != self.time:
+            raise AssertionError("adding log in inappropriate bucket")
+        self.logs.append(log)
+
+
+@dataclass
+class LogSeries:
+    """histogram like grouping of logs with time, using second intervals
+    eg. 
+        if start-time was 1:00:00
+        series[0] would be the logs captured in 1:00:00 - 1:00:01 interval
+        series[1] would be the logs captured in 1:00:01 - 1:00:02 interval
+    """
+
+    series: List[LogBucket] = field(default_factory=list)
+
+    @property
+    def start_time(self) -> Optional[datetime]:
+        if self.series:
+            return self.series[0].time
+
+    @property
+    def end_time(self) -> Optional[datetime]:
+        if self.series:
+            return self.series[-1].time
+
+    def append(self, log_bucket: LogBucket):
+        if not self.start_time:
+            self.series.append(log_bucket)
+            return
+
+        previous_bucket = self.series[-1]
+        if log_bucket.time - previous_bucket.time != timedelta(seconds=1):
+            raise AssertionError("invalid continuation log-bucket for series")
