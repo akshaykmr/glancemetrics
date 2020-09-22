4ff1bf638717ad2f67dc7a299748624928d41061
Akshay Kumar <akshay.kmr4321@gmail.com>
2020-09-22 12:59:25 +0530

    refactor

--- glancemetrics/watchdogs/log.py
+++ glancemetrics/watchdogs/log.py
@@ -8,40 +8,45 @@ def _needs_new_bucket(log: LogRecord, current_bucket: LogBucket) -> bool:
     return seconds_interval(log.time) != current_bucket.time
 
 
-def logwatcher(filepath: str):
-    """yields bucket of logs (groups of 1-second interval)"""
+def _validate_file_path(filepath: str) -> Path:
     path = Path(filepath).resolve()
     assert path.exists()
+    return path
+
+
+def logwatcher(log_file):
+    """yields bucket of logs (groups of 1-second interval)"""
     current_bucket = None
-    with open(str(path), "r") as log_file:
-        while True:
-            line = log_file.readline()
-            if not line:
-                # no new logs
-                yield None
-                sleep(0.2)
-            log = LogRecord.from_common_log_format(line)
-            assert log.time <= current_time()
+    while True:
+        line = log_file.readline()
+        if not line:
+            # no new logs
+            yield None
+            sleep(0.2)
+        log = LogRecord.from_common_log_format(line)
+        assert log.time <= current_time()
 
-            if current_bucket and _needs_new_bucket(log, current_bucket):
-                yield current_bucket
-                current_bucket = None
+        if current_bucket and _needs_new_bucket(log, current_bucket):
+            yield current_bucket
+            current_bucket = None
 
-            if current_bucket is None:
-                current_bucket = LogBucket(time=seconds_interval(log.time))
+        if current_bucket is None:
+            current_bucket = LogBucket(time=seconds_interval(log.time))
 
-            current_bucket.add(log)
+        current_bucket.add(log)
 
 
 if __name__ == "__main__":
     # experiment zone
     file_path = "logs.txt"
-    watcher = logwatcher(file_path)
-    limit = 10
-    count = 0
-    for bucket in watcher:
-        if count > limit:
-            break
-        print(bucket)
-        print("\n\n")
-        count += 1
+    path = _validate_file_path(file_path)
+    with open(str(path), "r") as log_file:
+        watcher = logwatcher(log_file)
+        limit = 10
+        count = 0
+        for bucket in watcher:
+            if count > limit:
+                break
+            print(bucket)
+            print("\n\n")
+            count += 1
